7a3a35ec9c81ab8d6c8f0b6190430dcaf0aabee8
Mikhail Kotov <efoxxi@gmail.com>
2011-06-07 04:57:26 +1000

    index page code

--- index.php
+++ index.php
@@ -1,29 +1,23 @@
 <?php
+$page_arr = array("deliverables",
+    "team",
+    "prototypes",
+    "client",
+    "project",
+    "contactus");
+
 include_once("inc/header.php");
 include_once("inc/menu.php");
 include_once("inc/body1.php");
-if(isset($_GET['page'])) {
-	if(strcmp($_GET['page'], "deliverables") == 0) {
-    	include_once("inc/content/deliverables.php");
-	}
-	if(strcmp($_GET['page'], "team") == 0) {
-    	include_once("inc/content/team.php");
-	}
-	if(strcmp($_GET['page'], "prototypes") == 0) {
-    	include_once("inc/content/prototypes.php");
-	}
-	if(strcmp($_GET['page'], "client") == 0) {
-    	include_once("inc/content/client.php");
-	}
-	if(strcmp($_GET['page'], "project") == 0) {
-    	include_once("inc/content/project.php");
-	}
-	if(strcmp($_GET['page'], "contactus") == 0) {
-    	include_once("inc/content/contactus.php");
-	}
+if (isset($_GET['page'])) {
+    foreach ($page_arr as $page) {
+        if (strcmp($_GET['page'], $page) == 0) {
+            include_once("inc/content/" . $page . ".php");
+        }
+    }
 } else {
-	// include by default
-	include_once("inc/content/deliverables.php");
+    // include by default
+    include_once("inc/content/deliverables.php");
 }
 include_once("inc/footer.php");
 ?>
